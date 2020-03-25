5268e57b32ab7de581b5e5b0f166f6b720778b3b
ירדן קרוק <yarden2k3@gmail.com>
2020-03-25 16:55:27 +0200

    checking if methods was the problem

--- script1.py
+++ script1.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/about/', methods=['GET'])
+@app.route('/about/')
 def about():
     return render_template("about.html")
 

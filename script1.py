0e8ac710ceac8c234fa37f87ec9624d55dc2d274
ירדן קרוק <yarden2k3@gmail.com>
2020-03-25 16:39:48 +0200

    methods is a list

--- script1.py
+++ script1.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/', methods='GET')
+@app.route('/', methods=['GET'])
 def home():
     return render_template("home.html")
 
-@app.route('/about/', methods='GET')
+@app.route('/about/', methods=['GET'])
 def about():
     return render_template("about.html")
 
